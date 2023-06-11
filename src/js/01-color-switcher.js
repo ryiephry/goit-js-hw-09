7a320b99854b55944a998b2251f8b70dfadc8724
ryiephry <ephraimpt2@gmail.com>
2023-06-11 18:14:35 -0400

    2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,38 @@
+const startBtn = document.getElementsByTagName("button")[0];
+const stopBtn = document.getElementsByTagName("button") [1];
+const body = document.body;
 
+startBtn.addEventListener("click", colorChangingF);
+
+function colorChangingF() {
+    setInterval(everySecond, 1000);
+}
+function everySecond() {
+            return body.style.backgroundColor = getRandomHexColor()
+} 
+    
+stopBtn.addEventListener("click", stopColorChange);
+
+function stopColorChange() {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+}
+
+
+
+console.log("hi")
+
+
+
+
+
+
+
+
+
+
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
