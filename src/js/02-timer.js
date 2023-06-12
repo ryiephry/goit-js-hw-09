fc49ba4fceb30be89bb61c970979b1cb58f085e5
ryiephry <ephraimpt2@gmail.com>
2023-06-12 13:51:33 -0400

    t3

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,19 +1,21 @@
-// import flatpickr from "flatpickr";
-// import "flatpickr/dist/flatpickr.min.css";
-//const flatpickr = require("flatpickr");
-
 import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
 
-const inputEl = document.getElementById("datetime-picker") //[datetime-picker]
-console.log(inputEl)
-
-
-// flatpickr(inputEl)
-
-flatpickr("inputEl", {});
+const inputEl = document.querySelector("input[type=text]");
 
 
 
+console.log("test")
 
 
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
 
+flatpickr(inputEl, options)
