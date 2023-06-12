fb2b38631eecd88e96089bd35622a865340a5e23
ryiephry <ephraimpt2@gmail.com>
2023-06-12 10:51:24 -0400

    done task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,38 +1,40 @@
-const startBtn = document.getElementsByTagName("button")[0];
-const stopBtn = document.getElementsByTagName("button") [1];
-const body = document.body;
 
-startBtn.addEventListener("click", colorChangingF);
+import { throttle } from 'lodash';
 
-function colorChangingF() {
-    setInterval(everySecond, 1000);
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const bodyEl = document.body.style;
+
+startBtn.addEventListener('click', throttle(onStartBtnClick, 1000));
+stopBtn.addEventListener('click', onStopBtnClick);
+
+
+let intervalId = null; 
+
+function onStartBtnClick() {
+ 
+  stopBtn.disabled = false;
+  startBtn.disabled = true;
+
+  // Start the interval to update the body color every 1000 milliseconds
+  intervalId = setInterval(updateBodyColor, 1000);
 }
-function everySecond() {
-            return body.style.backgroundColor = getRandomHexColor()
-} 
-    
-stopBtn.addEventListener("click", stopColorChange);
 
-function stopColorChange() {
-    startBtn.disabled = true;
-    stopBtn.disabled = false;
+// Function to handle the stop button click event
+function onStopBtnClick() {
+  // Clear the interval to stop updating the body color
+  clearInterval(intervalId); // is able to clear the interval bec inter
+
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
 }
 
-
-
-console.log("hi")
-
-
-
-
-
-
-
-
-
-
-
+function updateBodyColor() {
+  bodyEl.backgroundColor = getRandomHexColor();
+}
 
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0')}`;
 }
