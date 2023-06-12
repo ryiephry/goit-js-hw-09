280b7e5b16abe21c382ca975c3fe2652f5d82bc4
ryiephry <ephraimpt2@gmail.com>
2023-06-12 12:05:04 -0400

    t1

---
+++ src/js/02-timer.js
@@ -0,0 +1,19 @@
+// import flatpickr from "flatpickr";
+// import "flatpickr/dist/flatpickr.min.css";
+//const flatpickr = require("flatpickr");
+
+import flatpickr from "flatpickr";
+
+const inputEl = document.getElementById("datetime-picker") //[datetime-picker]
+console.log(inputEl)
+
+
+// flatpickr(inputEl)
+
+flatpickr(inputEl, {});
+
+
+
+
+
+
